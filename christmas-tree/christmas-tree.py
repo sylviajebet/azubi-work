90c5d4c513981cf37176ff618a96b486a8f90f79
sylviajebet <sylviajebett@gmail.com>
2023-02-21 13:40:25 +0300

    Tree blinking now

--- christmas-tree/christmas-tree.py
+++ christmas-tree/christmas-tree.py
@@ -1,35 +1,47 @@
 import random
 import time
+import os
 
+# clear console (uses cls if os is windows else uses clear)
+os.system('cls' if os.name == 'nt' else 'clear')
 # Python program to draw a christmas tree
 
-# Define tree size 
+# Define tree size
 tree_size = 12
 
 # Define a list of colors for the tree
 colors = ['\033[92m', '\033[91m', '\033[93m']
 
-# Draw the star at top of the tree
-star = '\033[93m' + '***'
-print(' '*(tree_size-2) + star)
+# start animation loop
+while True:
+    # Draw the star at top of the tree
+    print(' '*(tree_size-1) + colors[2] + "*")
+    print(' '*(tree_size-2) + colors[2] + "***")
 
-# Draw the tree
-for i in range(tree_size):
-    stars = ''
-    for j in range(2*i + 1):
-        color = random.choice(colors)
-        stars += color + '*'
-    print(' '*(tree_size - i - 1) + stars)
+    # Draw the tree
+    for i in range(tree_size):
+        stars = ''
+        for j in range(2*i + 1):
+            if j == 0:
+                # set yellow for first ellement
+                color = colors[2]
+                # choose random colors for all other elements
+            else:
+                color = random.choice(colors)
+            stars += color + '*'
+        print(' '*(tree_size - i - 1) + stars)
 
-# Draw the tree stand
-stem = 3
-for i in range(stem):
-    stem = color + '\033[92m'
-    print(((' ' *10) + stem)+ (('#' * 3)+ stem) + ((''* 4) + stem))
+    # Draw the tree stand
+    stem = 3
+    for i in range(stem):
+        stem = color + '\033[92m'
+        print(((' ' * 10) + stem) + (('#' * 3) + stem) + (('' * 4) + stem))
 
+    # Display the message
+    print('\033[91m\033[1m\nMerry Christmas!\n\033[0m')
 
-# Wait for a moment before showing the message
-time.sleep(2)
+    # Wait for a 4 seconds before reanimating
+    time.sleep(1)
 
-# Display the message
-print('\033[91m\033[1m\nMerry Christmas!\n\033[0m')
+    # clear console (uses cls if os is windows else uses clear)
+    os.system('cls' if os.name == 'nt' else 'clear')
